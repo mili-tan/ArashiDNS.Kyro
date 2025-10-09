f085302b6d24ca45b3eb731461c0ca2072c27029
Milkey Tan <24996957+mili-tan@users.noreply.github.com>
2025-10-09 13:09:44 +0100

    +Time

--- Program.cs
+++ Program.cs
@@ -183,7 +183,7 @@ namespace ArashiDNS.Kyro
             };
 
             await client.Zones.DnsRecords.AddAsync(domainConfig.ZoneId, newRecord);
-            if (FullConfig.LogLevel < 3) Console.WriteLine($"    - Updated {domainConfig.SubDomain} : {bestRecord.Content} ({bestRecord.Type})");
+            if (FullConfig.LogLevel < 3) Console.WriteLine($"    - Updated {domainConfig.SubDomain} : {bestRecord.Content} ({bestRecord.Type}) @ {DateTime.Now}");
         }
 
         static async Task<bool> IsRecordAccessible(DnsRecord record, DomainConfig domainConfig)
